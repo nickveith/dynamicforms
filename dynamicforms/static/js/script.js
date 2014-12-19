898c0ebdcbae6e6752afe604a986e1c2ed44ad54
Isaac Lee Morris <isaaclee@ilmatic.io>
2014-12-18 22:10:58 -0800

    add window load bootstrap point

--- dynamicforms/static/js/script.js
+++ dynamicforms/static/js/script.js
@@ -1,5 +1,9 @@
 (function($, window) {
-
-
-
+	$(window).load(function () {
+		/**
+		 * This is where the application starts.
+		 */
+		
+		console.log("Hello, Moto.");
+	})
 }).call(this, jQuery, window);
